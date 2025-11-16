201846c4f1fd0e7dec5e1c7edd265a038a11db04
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-16 05:49:32 +0000

    ALIAS

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return abs(number) % 10
-number = int(input())
-print(def(number))
+    a = abs(number) % 10
+    print(a, end='')
+    return a
