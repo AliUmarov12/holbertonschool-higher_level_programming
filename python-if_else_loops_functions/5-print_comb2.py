243da8867c33eea80a91dbf9d56b97eb25abab34
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-16 05:04:39 +0000

    ALIAS

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print(i)
+        print("{}".format(i))
     else:
-        print(str(i).zfill(2), end=',')
+        print("{:02}".format(i), end=',')
