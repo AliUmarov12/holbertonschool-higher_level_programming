a797bc94b48a05efb87a639227e8952fd091424b
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-19 13:36:37 +0000

    ALIAS

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list):
     for i in range my_list:
-        return "{:d}".format(i)
+        print("{:d}".format(i))
 my_list = list(input())
 print(print_list_integer(my_list))
