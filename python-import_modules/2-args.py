7422a2a3a31250871d7a7bb6a9562cea2655d3af
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-19 12:00:16 +0000

    ALIAS

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
-a = len(argv)
+a = len(argv) - 1
 if __name__ == "__main__":
     if  a == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(a))
     for i in range(a):
-        print("{}: {}".format(i+1, a+1))
+        print("{}: {}".format(i+1, argv[i+1]))
