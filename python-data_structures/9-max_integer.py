4dff4a697d97f4e857ab71614f126d9a31d801f6
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-21 08:50:10 +0000

    ALIAS

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list):
+    if len(my_list) == 0:
+        return None
+    else:
+        my_list = sorted(my_list)
+        max_value = my_list[len(my_list) - 1]
+        return max_value
