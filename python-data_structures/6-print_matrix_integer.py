69dd63844f41c8eb1434edb13d7a3b8340810039
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-21 06:38:09 +0000

    ALIAS

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix):
     for i in matrix:
-        print("{:d}".format(*range(*i)),end='')
+        for k in i:
+            if (i.index(k) + 1) % 3 == 0:
+                if not ((matrix.index(i) + 1) == len(matrix) and (i.index(k) + 1) == len(i)):
+                    print("{:d}".format(k), '\n')
+                else:
+                    print("{:d}".format(k))
+            else:
+                print("{:d}".format(k), end=' ')
