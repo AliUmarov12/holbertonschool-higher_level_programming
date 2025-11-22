d4d73954903a618ea5b0faf837c45fb2956d0d02
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-22 13:49:09 +0000

    ALIAS

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     my_list1 = my_list[:]
     b=my_list1.count(search)
     for i in range(b):
-        my_list1[my_list.index(search)] = replace
+        my_list1[my_list1.index(search)] = replace
     return my_list1
