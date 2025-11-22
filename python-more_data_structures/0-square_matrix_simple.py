28a86d7efe32f1162a91bade6eef0ba72c7da180
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-22 13:15:43 +0000

    ALIAS

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix):
-    new_matrix = list(matrix)
+    new_matrix = []
+    for j in matrix:
+        new_matrix += [j]
     for i in new_matrix:
         for k in range(len(i)):
             i[k] **= 2
