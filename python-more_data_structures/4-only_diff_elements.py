3aab79284f89e9bd2fd202aa0991299c75eb3d4a
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-22 14:19:17 +0000

    ALIAS

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -5,6 +5,7 @@ def only_diff_elements(set_1, set_2):
         for k in set_2:
             if i == k:
                 a += [k]
+    c = []
     for i in list(set_1) + list(set_2):
         for k in a:
             if not i == k:
