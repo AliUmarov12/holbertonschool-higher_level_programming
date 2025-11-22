dae7535b717c79896104f74846bfc01319a03f99
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-22 15:02:42 +0000

    ALIAS

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     a_dictionary1 = a_dictionary
-    a = sorted(a_dcitionary1)
+    a = sorted(a_dictionary1)
     for i in a:
         a_dictionary1[i] = a_dictionary1[i] * 2
     return a_dictionary1
