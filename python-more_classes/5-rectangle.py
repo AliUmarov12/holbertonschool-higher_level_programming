353d6d962c16066cad573dfc1bbd73c79c9d2dde
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-29 19:27:08 +0000

    ALIAS

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a Rectangle class with width, height, area, perimeter, printing, repr, and deletion message."""
 
+
 class Rectangle:
     """Class that defines a rectangle by width and height."""
 
