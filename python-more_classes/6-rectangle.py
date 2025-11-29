dcb00476e2f40b4d4f501ba16518e3b4ccd9efcd
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-29 19:40:59 +0000

    ALIAS

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -63,6 +63,6 @@ class Rectangle:
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        """Print a message when the instance is deleted and decrement counter."""
+        """Print a message """
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
