87cc41659d4e5be1e3f79b8874b02566109afa64
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-30 15:57:28 +0000

    ALIAS

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-Function that returns the dictionary description of a class instance
-with only simple data structures for JSON serialization.
-"""
+"""Function"""
 
 
 def class_to_json(obj):
-    """
-    Returns a dictionary 
-    """
+    """Returns a dictionary"""
     simple_types = (list, dict, str, int, bool)
     return {
         k: v
