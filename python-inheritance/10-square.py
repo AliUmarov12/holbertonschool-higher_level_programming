30a39f0a92437ef708a6cb670411a964ef64dbd7
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-30 14:34:00 +0000

    ALIAS

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""kjdfbjbdfjeh"""
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """kldfnv"""
+
+    def __init__(self, size):
+        """kdfvfinvirj"""
+        self.integer_validator('size', size) 
+        self.__size = size
+
+    def area():
+        """lkjnrfrntj"""
+        return self.__size ** 2
