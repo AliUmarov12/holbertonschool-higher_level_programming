77fd2003f836fcec55c0ceb903ac20396a80b9e0
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-30 14:40:00 +0000

    ALIAS

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,11 +9,11 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initialize square with validated size"""
-        # size müsbət tam olmalıdır — Rectangle-in parent classı BaseGeometry yoxlayır
+        # size
         self.integer_validator("size", size)
         self.__size = size
 
-        # Rectangle konstruktorunu width və height eyni olaraq çağırırıq
+        # Rectangle
         super().__init__(size, size)
 
     def area(self):
