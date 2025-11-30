546067d89211515d179e43fc0f02d10c31587b4e
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-30 10:49:36 +0000

    ALIAS

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""slknfjrnv"""
+
+
+class BaseGeometry:
+    """dknkrnr"""
+    def area(self):
+        """kdnknvkvn"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """jnedjerjfnerj"""
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
