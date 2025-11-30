488216bb04dafb83536715463683eb688b2efd8b
AliUmarov12 <aliumarov2481@gmail.com>
2025-11-30 15:49:38 +0000

    ALIAS

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+from pathlib import Path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+items = []
+
+#knedfkrkfnrfn
+if Path(filename).exists():
+    items = load_from_json_file(filename)
+
+# Komanda
+items.extend(sys.argv[1:])
+
+# Siyahi
+save_to_json_file(items, filename)
