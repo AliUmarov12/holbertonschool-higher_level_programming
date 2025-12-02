a6fdf59d9fb2ae0f916c3cfe4fc96fe8c4ce95af
AliUmarov12 <aliumarov2481@gmail.com>
2025-12-02 14:16:59 +0000

    ALIAS

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -6,12 +6,12 @@ import urllib.error
 import sys
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = sys.argv[1]
 
     request = urllib.request.Request(url, headers={'cfclearance': 'true'})
 
     try:
-        with urllib.request.urlopen(url) as f:
+        with urllib.request.urlopen(request) as f:
             r = f.read()
             print(r.decode("utf-8"))
 
