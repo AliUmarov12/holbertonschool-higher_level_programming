6fc578f956abe8aec45a8471f232ec29ffcaae33
AliUmarov12 <aliumarov2481@gmail.com>
2025-12-02 08:12:04 +0000

    ALIAS

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as f:
+        s = f.read()
+
+    print("Body response:")
+    print("    - type: {}".format(type(s)))
+    print("    - content: {}".format(s))
+    print("    - utf8 content: {}".format(s.decode("utf-8")))
