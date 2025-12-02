cb9115580f36f9eab4ff52567f4015dce7b8a115
AliUmarov12 <aliumarov2481@gmail.com>
2025-12-02 14:18:17 +0000

    ALIAS

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""dklhfjrhfvur"""
-
+"""
+Sends a request to a URL and prints the response body (utf-8 decoded).
+Handles HTTPError by printing: Error code: <status>
+"""
 import urllib.request
 import urllib.error
 import sys
@@ -8,12 +10,12 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url, headers={'cfclearance': 'true'})
+    request = urllib.request.Request(url)
 
     try:
-        with urllib.request.urlopen(request) as f:
-            r = f.read()
-            print(r.decode("utf-8"))
+        with urllib.request.urlopen(request) as response:
+            body = response.read()
+            print(body.decode("utf-8"))
 
-    except urllib.error.HTTPError as g:
-        print("Error code: ", g.code)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
