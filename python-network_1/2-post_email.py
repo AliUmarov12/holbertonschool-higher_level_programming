872d900a6692e2b7400160c9c31e96cd19313e2e
AliUmarov12 <aliumarov2481@gmail.com>
2025-12-02 11:41:22 +0000

    ALIAS

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,13 +8,12 @@ import sys
 
 
 if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-	url = sys.argv[1]
-	email = sys.argv[2]
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
 
-	data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+    req = urllib.request.Request(url, data = data)
 
-	req = urllib.request.Request(url, data = data)
-
-	with urllib.request.urlopen(req) as f:
-		print(f.read().decode("utf-8"))
+    with urllib.request.urlopen(req) as f:
+        print(f.read().decode("utf-8"))
