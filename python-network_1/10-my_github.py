dd40a9e333e84848a2fe07032b98d6205a5dd730
AliUmarov12 <aliumarov2481@gmail.com>
2025-12-04 11:56:20 +0000

    ALIAS

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     url = "https://api.github.com/user"
-    abc = requests.get(url, acc=(username, password))
+    abc = requests.get(url, auth=(username, password))
     try:
         abcd = abc.json()
         print(abcd.get("id"))
