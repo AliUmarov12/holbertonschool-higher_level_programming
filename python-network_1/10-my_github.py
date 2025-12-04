d2a7efaee17ee32804852291752310e93ce2bb14
AliUmarov12 <aliumarov2481@gmail.com>
2025-12-04 11:50:07 +0000

    ALIAS

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""kldjvivi"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+    abc = requests.get(url, acc=(username, password))
+    try:
+        abcd = abc.json()
+        print(abcd.get("id"))
+    except:
+        print(None)
